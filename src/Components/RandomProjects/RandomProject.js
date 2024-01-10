9524e1ba27c4d0d483c1e94dc1d72dfdce361c49
Mushfig <mushfigaliyarov@gmail.com>
2024-01-11 02:22:18 +0400

    end

--- src/Components/RandomProjects/RandomProject.js
+++ src/Components/RandomProjects/RandomProject.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-
+import "./RandomProjectStyle.css"
 function RandomProject() {
-    const [minVal,setMinVal]=useState(0);
-    const [maxVal,setMaxVal]=useState(0);
-    const [randomVal,setRandomVal]=useState(0);
+    const [minVal,setMinVal]=useState();
+    const [maxVal,setMaxVal]=useState();
+    const [randomVal,setRandomVal]=useState();
  
   function getRandom(){
       setRandomVal(Math.floor(Math.random()*(maxVal-minVal -1) + minVal +1))
@@ -11,23 +11,24 @@ function RandomProject() {
 
 
   return (
-    <div className="Container w-[100%] h-[100vh] flex justify-center items-center bg-[#DED0B6]">
+    <div className="Container w-[100%] h-[100vh] flex flex-col justify-center items-center bg-[#DED0B6]">
+      <h1 className="text-[38px] font-bold mb-[60px] bg-[#B2A59B] py-[10px] px-[10px] text-center rounded-[5px] w-[30%]">Random Number Generator</h1>
       <div className="main w-[30%] flex flex-col justify-center items-center  h-[500px] bg-[#607274] rounded-[10px] ">
         <div className="mainDiv flex h-[450px] w-[80%] flex-col justify-evenly ">
           <div className="randomDiv bg-[#FAEED1] px-[5px] py-[10px] font-bold text-[20px]">
             <p>Random Value: {randomVal}</p>
           </div>
           <div className="minMaxDiv flex justify-between w-[100%] ">
-            <div className="minDiv flex flex-col gap-[5px] h-[60px] w-[45%] font-bold text-[18px]">
-              <p>Minimum Value: {minVal}</p>
-              <input value={minVal} onChange={e=>setMinVal(Number(e.target.value))} type="number" className="px-[5px] bg-[#FAEED1] h-[30px] w-[100%]" />
+            <div className="minDiv flex flex-col gap-[5px] h-[60px] w-[45%] font-bold text-[20px]">
+              <p>Minimum Value</p>
+              <input value={minVal} onChange={e=>setMinVal(Number(e.target.value))} type="number" className="px-[5px] bg-[#FAEED1] w-[100%]" />
             </div>
-            <div className="maxDiv flex flex-col gap-[5px] h-[60px] w-[45%] font-bold text-[18px]">
-              <p>Maximum Value: {maxVal}</p>
-              <input value={maxVal} onChange={e=>setMaxVal(Number(e.target.value))} type="number" className="px-[5px] bg-[#FAEED1] h-[30px] w-[100%]" />
+            <div className="maxDiv flex flex-col gap-[5px] h-[60px] w-[45%] font-bold text-[20px]">
+              <p>Maximum Value</p>
+              <input value={maxVal} onChange={e=>setMaxVal(Number(e.target.value))} type="number" className="px-[5px] bg-[#FAEED1] w-[100%]" />
             </div>
           </div>
-          <button onClick={getRandom} className="bg-[#FAEED1] py-[13px] font-bold text-[20px]">Get Random Number.....</button>
+          <button onClick={getRandom} className="bg-[#FAEED1] py-[13px] font-bold text-[25px] hover:bg-[#DED0B6]">Get Random Number</button>
 
         </div>
       </div>
